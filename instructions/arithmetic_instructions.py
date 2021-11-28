a9f9d08a695ba648e76e405868e65ccade9e0ba0
Fernando Zanutto <ferzanutto1999@gmail.com>
2021-11-28 20:56:02 -0300

    Add CMP instruction and renamed status b flag

--- instructions/arithmetic_instructions.py
+++ instructions/arithmetic_instructions.py
@@ -2,16 +2,34 @@
 
 from addressing import ImmediateReadAddressing
 from instructions.generic_instructions import Instruction
+from status import Status
 
 
 class And(Instruction):
-    identifier_byte = bytes([0x29])
     sets_zero_bit = True
     sets_negative_bit = True
 
 
+class Cmp(Instruction):
+    @classmethod
+    def apply_side_effects(cls, cpu, memory_address, value):
+        diff = cpu.a_reg - value
+        cpu.status_reg.bits[Status.StatusTypes.carry] = diff >= 0
+        cpu.status_reg.bits[Status.StatusTypes.zero] = diff == 0
+        cpu.status_reg.bits[Status.StatusTypes.negative] = cpu.a_reg >= 0x80
+
+
 class AndImm(ImmediateReadAddressing, And):
+    identifier_byte = bytes([0x29])
+
+    @classmethod
+    def get_data(cls, cpu, memory_address, data_bytes):
+        return cpu.a_reg & super().get_data(cpu, memory_address, data_bytes)
 
     @classmethod
     def write(cls, cpu, memory_address, value):
-        cpu.a_reg = cpu.a_reg & value
+        cpu.a_reg = value
+
+
+class CmpImm(ImmediateReadAddressing, Cmp):
+    identifier_byte = bytes([0xC9])

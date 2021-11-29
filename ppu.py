d033e7f7336fbbc136e1b334fcd2f428cd0ebba8
Fernando Zanutto <ferzanutto1999@gmail.com>
2021-11-28 21:03:08 -0300

    PPU brief documentation

--- ppu.py
+++ ppu.py
@@ -2,6 +2,16 @@ from memory_owner import MemoryOwnerMixin
 
 
 class PPU(MemoryOwnerMixin, object):
+    '''
+    $2000 -> PPUCTRL: PPU control register. Access: write
+    $2001 -> PPUMASK: PPu mask register. Access: write
+    $2002 -> PPUSTATUS: PPU status register. Access: read
+    $2003 -> OAMADDR: OAM address port. Access: write
+    $2004 -> OAMDATA: OAM data port. Access: read, write
+    $2005 -> PPUSCROLL: PPU scrolling position register. Access: write twice
+    $2006 -> PPUADDR: PPU address register. Access: write twice
+    $2007 -> PPUDATA: PPU data register. Access: read, write
+    '''
     memory_start_location = 0x2000
     memory_end_location = 0x2007
 

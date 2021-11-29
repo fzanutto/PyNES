b9e46af3959aea77d81fdff3eee9a1a646b6ff1e
Fernando Zanutto <ferzanutto1999@gmail.com>
2021-11-28 22:55:12 -0300

    Add Or and Xor instruction

--- instructions/arithmetic_instructions.py
+++ instructions/arithmetic_instructions.py
@@ -1,6 +1,7 @@
 
 
-from addressing import ImmediateReadAddressing
+from typing import Optional
+from addressing import AbsoluteAddressing, ImmediateReadAddressing, ZeroPageAddressing, ZeroPageAddressingWithX
 from instructions.generic_instructions import Instruction
 from status import Status
 
@@ -8,6 +9,18 @@ from status import Status
 class And(Instruction):
     sets_zero_bit = True
     sets_negative_bit = True
+
+    @classmethod
+    def write(cls, cpu, memory_address, value):
+        cpu.a_reg = value
+
+
+class AndImm(ImmediateReadAddressing, And):
+    identifier_byte = bytes([0x29])
+
+    @classmethod
+    def get_data(cls, cpu, memory_address, data_bytes):
+        return cpu.a_reg & super().get_data(cpu, memory_address, data_bytes)
 
 
 class Cmp(Instruction):
@@ -19,17 +32,35 @@ class Cmp(Instruction):
         cpu.status_reg.bits[Status.StatusTypes.negative] = cpu.a_reg >= 0x80
 
 
-class AndImm(ImmediateReadAddressing, And):
-    identifier_byte = bytes([0x29])
+class CmpImm(ImmediateReadAddressing, Cmp):
+    identifier_byte = bytes([0xC9])
+
+
+class Or(Instruction):
+    sets_zero_bit = True
+    sets_negative_bit = True
 
     @classmethod
-    def get_data(cls, cpu, memory_address, data_bytes):
-        return cpu.a_reg & super().get_data(cpu, memory_address, data_bytes)
+    def write(cls, cpu, memory_address, value):
+        cpu.a_reg = cpu.a_reg | value
+
+
+class OrImm(ImmediateReadAddressing, Or):
+    identifier_byte = bytes([0x09])
+
+
+class Xor(Instruction):
+    sets_zero_bit = True
+    sets_negative_bit = True
 
     @classmethod
     def write(cls, cpu, memory_address, value):
         cpu.a_reg = value
 
 
-class CmpImm(ImmediateReadAddressing, Cmp):
-    identifier_byte = bytes([0xC9])
+class XorImm(ImmediateReadAddressing, Xor):
+    identifier_byte = bytes([0x49])
+
+    @classmethod
+    def get_data(cls, cpu, memory_address, data_bytes):
+        return cpu.a_reg ^ super().get_data(cpu, memory_address, data_bytes)

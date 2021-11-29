8af52d99dc58f33bd9166fc23ac59fbdc2ec01eb
Fernando Zanutto <ferzanutto1999@gmail.com>
2021-11-28 23:45:30 -0300

    Added ADC instruction and fixed CMP instruction

--- instructions/arithmetic_instructions.py
+++ instructions/arithmetic_instructions.py
@@ -29,7 +29,7 @@ class Cmp(Instruction):
         diff = cpu.a_reg - value
         cpu.status_reg.bits[Status.StatusTypes.carry] = diff >= 0
         cpu.status_reg.bits[Status.StatusTypes.zero] = diff == 0
-        cpu.status_reg.bits[Status.StatusTypes.negative] = cpu.a_reg >= 0x80
+        cpu.status_reg.bits[Status.StatusTypes.negative] = diff & (1 << 7)
 
 
 class CmpImm(ImmediateReadAddressing, Cmp):
@@ -64,3 +64,38 @@ class XorImm(ImmediateReadAddressing, Xor):
     @classmethod
     def get_data(cls, cpu, memory_address, data_bytes):
         return cpu.a_reg ^ super().get_data(cpu, memory_address, data_bytes)
+
+
+class AddCarry(Instruction):
+    sets_zero_bit = True
+    sets_negative_bit = True
+
+    @classmethod
+    def write(cls, cpu, memory_address, value):
+        cpu.a_reg = value
+
+
+class AddCarryImm(ImmediateReadAddressing, AddCarry):
+    identifier_byte = bytes([0x69])
+
+    @classmethod
+    def get_data(cls, cpu, memory_address, data_bytes):
+        value = super().get_data(cpu, memory_address, data_bytes)
+        is_first_number_positive = cpu.a_reg < 128
+        is_second_number_positive = value < 128
+
+        sum = cpu.a_reg + value + \
+            int(cpu.status_reg.bits[Status.StatusTypes.carry])
+
+        cpu.status_reg.bits[Status.StatusTypes.carry] = sum > 255
+
+        sum = sum % 256
+
+        is_sum_positive = sum < 128
+
+        if is_first_number_positive == is_second_number_positive:
+            cpu.status_reg.bits[Status.StatusTypes.overflow] = is_first_number_positive != is_sum_positive
+        else:
+            cpu.status_reg.bits[Status.StatusTypes.overflow] = False
+
+        return sum

102223e760eafd0cfbfb0d21c81614b34a9ed2fe
Fernando Zanutto <ferzanutto1999@gmail.com>
2021-12-04 10:08:48 -0300

    Finished all instrctions

--- instructions/unofficial_instructions.py
+++ instructions/unofficial_instructions.py
@@ -79,6 +79,7 @@ class SbcNop(ImmediateReadAddressing, Sbc):
 class Dcp(Instruction):
     @classmethod
     def apply_side_effects(cls, cpu, memory_address, value):
+        value = cpu.a_reg - value
         cpu.status_reg.bits[Status.StatusTypes.carry] = value >= 0
         cpu.status_reg.bits[Status.StatusTypes.zero] = value == 0
         cpu.status_reg.bits[Status.StatusTypes.negative] = (
@@ -88,6 +89,10 @@ class Dcp(Instruction):
     def get_data(cls, cpu, memory_address, data_bytes) -> int:
         value = cpu.get_memory(memory_address)
         return value - 1 if value > 0 else 255
+
+    @classmethod
+    def write(cls, cpu, memory_address, value):
+        cpu.set_memory(memory_address, value)
 
 
 class DcpZeroPage(ZeroPageAddressing, Dcp):
@@ -116,3 +121,305 @@ class DcpIdxInd(IndexedIndirectAddressing, Dcp):
 
 class DcpIndIdx(IndirectIndexedAddressing, Dcp):
     identifier_byte = bytes([0xD3])
+
+
+class Isc(Instruction):
+    sets_zero_bit = True
+    sets_negative_bit = True
+
+    @classmethod
+    def get_data(cls, cpu, memory_address, data_bytes) -> int:
+        mem_value = (cpu.get_memory(memory_address) + 1) % 256
+        cpu.set_memory(memory_address, mem_value)
+
+        mem_value = (~mem_value) & 255
+
+        return cls.sub_carry(cpu, memory_address, data_bytes, mem_value)
+
+    @classmethod
+    def write(cls, cpu, memory_address, value):
+        cpu.a_reg = value
+
+    def sub_carry(cpu, memory_address, data_bytes, value):
+        is_first_number_positive = cpu.a_reg < 128
+        is_second_number_positive = value < 128
+
+        sub = cpu.a_reg + value + \
+            int(cpu.status_reg.bits[Status.StatusTypes.carry])
+
+        cpu.status_reg.bits[Status.StatusTypes.carry] = sub > 255
+
+        sub = sub % 256
+
+        is_sum_positive = sub < 128
+
+        if is_first_number_positive == is_second_number_positive:
+            cpu.status_reg.bits[Status.StatusTypes.overflow] = is_first_number_positive != is_sum_positive
+        else:
+            cpu.status_reg.bits[Status.StatusTypes.overflow] = False
+
+        return sub
+
+
+class IscZeroPage(ZeroPageAddressing, Isc):
+    identifier_byte = bytes([0xE7])
+
+
+class IscZeroPageX(ZeroPageAddressingWithX, Isc):
+    identifier_byte = bytes([0xF7])
+
+
+class IscAbs(AbsoluteAddressing, Isc):
+    identifier_byte = bytes([0xEF])
+
+
+class IscAbsX(AbsoluteAddressingWithX, Isc):
+    identifier_byte = bytes([0xFF])
+
+
+class IscAbsY(AbsoluteAddressingWithY, Isc):
+    identifier_byte = bytes([0xFB])
+
+
+class IscIdxInd(IndexedIndirectAddressing, Isc):
+    identifier_byte = bytes([0xE3])
+
+
+class IscIndIdx(IndirectIndexedAddressing, Isc):
+    identifier_byte = bytes([0xF3])
+
+
+class Slo(Instruction):
+    sets_zero_bit = True
+    sets_negative_bit = True
+
+    def asl(cpu, value):
+        cpu.status_reg.bits[Status.StatusTypes.carry] = value & (1 << 7) > 0
+        value = (value << 1) & 255
+        return value
+
+    @classmethod
+    def get_data(cls, cpu, memory_address, data_bytes) -> int:
+        value = cpu.get_memory(memory_address)
+        value = cls.asl(cpu, value)
+
+        cpu.set_memory(memory_address, value, num_bytes=1)
+
+        return value | cpu.a_reg
+
+    @classmethod
+    def write(cls, cpu, memory_address, value):
+        cpu.a_reg = value
+
+
+class SloZeroPage(ZeroPageAddressing, Slo):
+    identifier_byte = bytes([0x07])
+
+
+class SloZeroPageX(ZeroPageAddressingWithX, Slo):
+    identifier_byte = bytes([0x17])
+
+
+class SloAbsolute(AbsoluteAddressing, Slo):
+    identifier_byte = bytes([0x0F])
+
+
+class SloAbsoluteX(AbsoluteAddressingWithX, Slo):
+    identifier_byte = bytes([0x1F])
+
+
+class SloAbsoluteY(AbsoluteAddressingWithY, Slo):
+    identifier_byte = bytes([0x1B])
+
+
+class SloIdxInd(IndexedIndirectAddressing, Slo):
+    identifier_byte = bytes([0x03])
+
+
+class SloIndIdx(IndirectIndexedAddressing, Slo):
+    identifier_byte = bytes([0x13])
+
+
+class Rla(Instruction):
+    sets_zero_bit = True
+    sets_negative_bit = True
+
+    @classmethod
+    def get_data(cls, cpu, memory_address, data_bytes) -> int:
+        value = cpu.get_memory(memory_address)
+
+        value = cls.rol(cpu, value)
+
+        cpu.set_memory(memory_address, value)
+
+        return cpu.a_reg & value
+
+    def rol(cpu, value):
+        current_carry = cpu.status_reg.bits[Status.StatusTypes.carry]
+
+        cpu.status_reg.bits[Status.StatusTypes.carry] = value & (1 << 7) > 1
+
+        value = ((value << 1) | current_carry) & 255
+
+        return value
+
+    @classmethod
+    def write(cls, cpu, memory_address, value):
+        cpu.a_reg = value
+
+
+class RlaZeroPage(ZeroPageAddressing, Rla):
+    identifier_byte = bytes([0x27])
+
+
+class RlaZeroPageX(ZeroPageAddressingWithX, Rla):
+    identifier_byte = bytes([0x37])
+
+
+class RlaAbsolute(AbsoluteAddressing, Rla):
+    identifier_byte = bytes([0x2F])
+
+
+class RlaAbsoluteX(AbsoluteAddressingWithX, Rla):
+    identifier_byte = bytes([0x3F])
+
+
+class RlaAbsoluteY(AbsoluteAddressingWithY, Rla):
+    identifier_byte = bytes([0x3B])
+
+
+class RlaIdxInd(IndexedIndirectAddressing, Rla):
+    identifier_byte = bytes([0x23])
+
+
+class RlaIndIdx(IndirectIndexedAddressing, Rla):
+    identifier_byte = bytes([0x33])
+
+
+class Sre(Instruction):
+    sets_zero_bit = True
+    sets_negative_bit = True
+
+    @classmethod
+    def write(cls, cpu, memory_address, value):
+        cpu.a_reg = value
+
+    def lsr(cpu, value):
+        cpu.status_reg.bits[Status.StatusTypes.carry] = value & 0x1
+
+        value = value >> 1
+
+        return value
+
+    @classmethod
+    def get_data(cls, cpu, memory_address, data_bytes) -> int:
+        value = cpu.get_memory(memory_address)
+        value = cls.lsr(cpu, value)
+
+        cpu.set_memory(memory_address, value)
+
+        return cpu.a_reg ^ value
+
+
+class SreZeroPage(ZeroPageAddressing, Sre):
+    identifier_byte = bytes([0x47])
+
+
+class SreZeroPageX(ZeroPageAddressingWithX, Sre):
+    identifier_byte = bytes([0x57])
+
+
+class SreAbsolute(AbsoluteAddressing, Sre):
+    identifier_byte = bytes([0x4F])
+
+
+class SreAbsoluteX(AbsoluteAddressingWithX, Sre):
+    identifier_byte = bytes([0x5F])
+
+
+class SreAbsoluteY(AbsoluteAddressingWithY, Sre):
+    identifier_byte = bytes([0x5B])
+
+
+class SreIdxInd(IndexedIndirectAddressing, Sre):
+    identifier_byte = bytes([0x43])
+
+
+class SreIndIdx(IndirectIndexedAddressing, Sre):
+    identifier_byte = bytes([0x53])
+
+
+class Rra(Instruction):
+    sets_zero_bit = True
+    sets_negative_bit = True
+
+    @classmethod
+    def get_data(cls, cpu, memory_address, data_bytes) -> int:
+        value = cpu.get_memory(memory_address)
+
+        value = cls.ror(cpu, value)
+
+        cpu.set_memory(memory_address, value)
+
+        return cls.add_carry(cpu, value)
+
+    def ror(cpu, value):
+        current_carry = cpu.status_reg.bits[Status.StatusTypes.carry]
+
+        cpu.status_reg.bits[Status.StatusTypes.carry] = value & 0x1
+
+        value = (value >> 1) | (current_carry << 7)
+
+        return value
+
+    def add_carry(cpu, value):
+        is_first_number_positive = cpu.a_reg < 128
+        is_second_number_positive = value < 128
+
+        sum = cpu.a_reg + value + \
+            int(cpu.status_reg.bits[Status.StatusTypes.carry])
+
+        cpu.status_reg.bits[Status.StatusTypes.carry] = sum > 255
+
+        sum = sum % 256
+
+        is_sum_positive = sum < 128
+
+        if is_first_number_positive == is_second_number_positive:
+            cpu.status_reg.bits[Status.StatusTypes.overflow] = is_first_number_positive != is_sum_positive
+        else:
+            cpu.status_reg.bits[Status.StatusTypes.overflow] = False
+
+        return sum
+
+    @classmethod
+    def write(cls, cpu, memory_address, value):
+        cpu.a_reg = value
+
+
+class RraZeroPage(ZeroPageAddressing, Rra):
+    identifier_byte = bytes([0x67])
+
+
+class RraZeroPageX(ZeroPageAddressingWithX, Rra):
+    identifier_byte = bytes([0x77])
+
+
+class RraAbsolute(AbsoluteAddressing, Rra):
+    identifier_byte = bytes([0x6F])
+
+
+class RraAbsoluteX(AbsoluteAddressingWithX, Rra):
+    identifier_byte = bytes([0x7F])
+
+
+class RraAbsoluteY(AbsoluteAddressingWithY, Rra):
+    identifier_byte = bytes([0x7B])
+
+
+class RraIdxInd(IndexedIndirectAddressing, Rra):
+    identifier_byte = bytes([0x63])
+
+
+class RraIndIdx(IndirectIndexedAddressing, Rra):
+    identifier_byte = bytes([0x73])

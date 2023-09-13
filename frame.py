b13d71c699db0c9702515494a03fcb83bf82ac17
Fernando Zanutto <fernandozanutto@outlook.com>
2023-09-13 00:03:01 -0300

    fix type hint

--- frame.py
+++ frame.py
@@ -5,7 +5,7 @@ class Frame:
     def __init__(self) -> None:
         self.data = [(0,0,0)] * Frame.WIDTH * Frame.HEIGHT
 
-    def set_pixel(self, x: int, y: int, rgb: tuple[3]):
+    def set_pixel(self, x: int, y: int, rgb: list[int]):
         position = y * Frame.WIDTH + x
         if position < 256 * 240:
             self.data[position] = rgb
